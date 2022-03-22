52546e2ad0ec36538a3e8056759d3b3042ca3b94
gh-actions <gh-actions@noreply.github.com>
2022-03-22 00:41:43 +0000

    Initialized ergodox_steel-default-right-bluemicro_nrf52840 using GitHub Actions

--- ergodox_steel-default-right-bluemicro_nrf52840/keymap.cpp
+++ ergodox_steel-default-right-bluemicro_nrf52840/keymap.cpp
@@ -25,25 +25,25 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
  */
  std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
     {KEYMAP( /* qwerty */
-      KC_EQL,   KC_1,     KC_2,    KC_3,     KC_4,    KC_5,  KC_6,    \
-      KC_TAB,   KC_Q,     KC_W,    KC_E,     KC_R,    KC_T,  KC_LBRC,  \
-      KC_LCTL,  KC_A,     KC_S,    KC_D,     KC_F,    KC_G,  _______, \
-      KC_LSFT,  KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,  _______,  \
-      KC_ESC,   KC_GRV,   KC_LBRC, KC_LSFT,  KC_LGUI, KC_ENT, _______, \
-      KC_1, KC_F11, KC_DEL, KC_BSPC, MK(KC_T), KC_LGUI, KC_LALT \
-    )};
+       KC_EQL,   KC_1,     KC_2,    KC_3,     KC_4,    KC_5,  BLEPROFILE_1,    \
+       KC_TAB,   KC_Q,     KC_W,    KC_E,     KC_R,    KC_T,  KC_LBRC,  \
+       KC_LCTL,  KC_A,     KC_S,    KC_D,     KC_F,    KC_G,  _______, \
+       KC_LSFT,  KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,  _______,  \
+       KC_ESC,   KC_GRV,   KC_LBRC, KC_LSFT,  KC_LGUI, KC_ENT, _______, \
+       KC_1, BLEPROFILE_2, KC_DEL, KC_BSPC, KC_VOLU, KC_LGUI, KC_LALT \
+     )};
 
 void setupKeymap() {
 
     uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
     KEYMAP( /* qwerty */
-      KC_EQL,   KC_1,     KC_2,    KC_3,     KC_4,    KC_5,  KC_6,    \
-      KC_TAB,   KC_Q,     KC_W,    KC_E,     KC_R,    KC_T,  KC_LBRC,  \
-      KC_LCTL,  KC_A,     KC_S,    KC_D,     KC_F,    KC_G,  _______, \
-      KC_LSFT,  KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,  _______,  \
-      KC_ESC,   KC_GRV,   KC_LBRC, KC_LSFT,  KC_LGUI, KC_ENT, _______, \
-      KC_1, KC_F11, KC_DEL, KC_BSPC, MK(KC_T), KC_LGUI, KC_LALT \
-    );
+       KC_EQL,   KC_1,     KC_2,    KC_3,     KC_4,    KC_5,  BLEPROFILE_1,    \
+       KC_TAB,   KC_Q,     KC_W,    KC_E,     KC_R,    KC_T,  KC_LBRC,  \
+       KC_LCTL,  KC_A,     KC_S,    KC_D,     KC_F,    KC_G,  _______, \
+       KC_LSFT,  KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,  _______,  \
+       KC_ESC,   KC_GRV,   KC_LBRC, KC_LSFT,  KC_LGUI, KC_ENT, _______, \
+       KC_1, BLEPROFILE_2, KC_DEL, KC_BSPC, KC_VOLU, KC_LGUI, KC_LALT \
+     );
 
 
      /*
@@ -62,7 +62,7 @@ void setupKeymap() {
 
  std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
      {KEYMAP( /* qwerty */
-       KC_MUTE, KC_6,   KC_7,   KC_8,   KC_9,   KC_0,  KC_MINUS,   \
+       KC_NO, KC_6,   KC_7,   KC_8,   KC_9,   KC_0,  KC_MINUS,   \
        KC_RBRC,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,  KC_BSLS,  \
        KC_RBRC, KC_H,   KC_J,   KC_K,   KC_L,   KC_SCOLON,  KC_QUOTE, \
        KC_NO,   KC_N,   KC_M, KC_COMMA, KC_DOT, KC_SLASH,   KC_LSFT,  \
@@ -73,9 +73,9 @@ void setupKeymap() {
  void setupKeymap() {
 
      uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
-     KEYMAP(
-       KC_MUTE, KC_6,   KC_7,   KC_8,   KC_9,   KC_0,  KC_MINUS,   \
-       KC_NO,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,  KC_BSLS,  \
+     KEYMAP( /* qwerty */
+       KC_NO, KC_6,   KC_7,   KC_8,   KC_9,   KC_0,  KC_MINUS,   \
+       KC_RBRC,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,  KC_BSLS,  \
        KC_RBRC, KC_H,   KC_J,   KC_K,   KC_L,   KC_SCOLON,  KC_QUOTE, \
        KC_NO,   KC_N,   KC_M, KC_COMMA, KC_DOT, KC_SLASH,   KC_LSFT,  \
        KC_NO,   KC_NO, KC_LEFT,   KC_UP, KC_RIGHT, KC_DOWN, KC_LALT,   \
